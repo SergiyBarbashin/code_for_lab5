9ab3dfcc587ef7a98918c39a440fa35f7a3fbc34
hok <sergiy_barbashin@protonmail.com>
2022-10-04 15:38:11 +0300

    main_commit_lab5

---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab5
+{
+     class Program
+    {
+        interface IStructure
+        {
+            void Connect(Cable cable);
+            void Connect(Server server);
+            void Connect(Workstation workstation);
+        }
+
+        interface INetwork
+        {
+            void Accept(IStructure s);
+        };
+
+        class Cable : INetwork
+        {
+            public void Accept(IStructure s) => s.Connect(this);
+        }
+
+        class Server : INetwork
+        {
+            public void Accept(IStructure s) => s.Connect(this);
+        }
+        class Workstation : INetwork
+        {
+            public void Accept(IStructure s) => s.Connect(this);
+        }
+
+        class NetStructure: IStructure
+        {
+            public int estimation = 0;
+            public string result;
+            public void Connect(Cable c)
+            {
+                result = "Cable is connected!";
+                estimation += 10;
+            }
+            public void Connect(Server c)
+            {
+                result = "Server is provided!";
+                estimation += 50;
+            }
+            public void Connect(Workstation c)
+            {
+                result = "Workstation is configured!";
+                estimation += 100;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Visitor
+            List<INetwork> network = new List<INetwork>() {new Cable(), new Server(), new Workstation(), new Cable(), new Workstation() };
+            NetStructure set = new NetStructure();
+            foreach (INetwork x in network)
+            {
+                x.Accept(set);
+                Console.WriteLine(set.result);
+            }
+            Console.WriteLine("\nStructure estimation: " + set.estimation+"$");
+            Console.ReadLine();
+        }
+    }
+}
